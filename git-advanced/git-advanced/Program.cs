ef15d0754c78b646f1996a64c948488c9ac10ba1
dangm <dang.m@seznam.cz>
2019-12-02 15:08:33 +0100

    b4 and b5

--- git-advanced/git-advanced/Program.cs
+++ git-advanced/git-advanced/Program.cs
@@ -13,20 +13,12 @@ namespace gitadvanced
 
         public static void Cheer(int numberOfMembers)
         {
-<<<<<<< HEAD
-            Console.WriteLine("C#");
-=======
-            Console.WriteLine($"We are React team! With {numberOfMembers} members.");
->>>>>>> 711ce507812621b45a7696990b8d4bd366105bb4
+
         }
 
-        public  void Count(string message)
+        public  void Count(string message, string message2)
         {
-<<<<<<< HEAD
-            Console.WriteLine("May!");
-=======
-            Console.WriteLine(message);
->>>>>>> 86c36906575381612f168ed79a17b54dd5882b4d
+            Console.WriteLine(message + message2);
         }
 
         public void Future(string name)
