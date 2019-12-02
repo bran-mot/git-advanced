cb1274ac8534b2ce050f388ffa5afec373eee080
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-12-02 14:10:23 +0100

    a1

---
+++ git-advanced/git-advanced/BrandonsClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace gitadvanced
+{
+    public class BrandonsClass
+    {
+        public static void greet()
+        {
+            Console.WriteLine("what up, loser?");
+        }
+    }
+}
