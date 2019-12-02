333ff723ae2522b4dd1b142840aac1ea50c65b28
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-12-02 14:05:32 +0100

    New project

---
+++ git-advanced/git-advanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitadvanced
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
